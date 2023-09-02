b28c2c2e378368c2dac8a5fafc11b65fd53a7624
Arvin Josol <arvin.josol@live.com>
2023-09-02 16:50:34 -0700

    add JS to create default comments - remove dummy comment html in index.html - add array holding the default comment as objects     - object has to have these properties:         - username         - timestamp         - text - create function to display all comments in the array

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,37 +1,64 @@
-const commentsForm = document.querySelector(".comments");
+const commentsArray = [
+    {
+        username: "Miles Acosta",
+        timestamp: "12/20/2020",
+        text: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough."
+    },
 
-const commentsCard = document.createElement("div");
-commentsCard.setAttribute("class", "comments__card")
+    {
+        username: "Emilie Beach",
+        timestamp: "01/09/2021",
+        text: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day."
+    },
 
-commentsForm.appendChild(commentsCard);
+    {
+        username: "Connor Walton",
+        timestamp: "02/17/2021",
+        text: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains."
+    }
 
-const commentsAvatar = document.createElement("div");
-commentsAvatar.setAttribute("class", "comments__avatar");
-commentsCard.appendChild(commentsAvatar);
+];
 
-const commentsContentContainer = document.createElement("div");
-commentsContentContainer.setAttribute("class", "comments__content-container")
-commentsCard.appendChild(commentsContentContainer);
+const displayComment = (arrayItem) => {
+    const commentsForm = document.querySelector(".comments");
 
-const commentsData = document.createElement("div");
-commentsData.setAttribute("class", "comments__data");
-commentsContentContainer.appendChild(commentsData);
+    const commentsCard = document.createElement("div");
+    commentsCard.setAttribute("class", "comments__card")
 
-const commentsUsername = document.createElement("p");
-commentsUsername.setAttribute("class", "comments__username");
-commentsUsername.innerText = "Connor Walton";
-commentsData.appendChild(commentsUsername);
+    commentsForm.appendChild(commentsCard);
 
-const commentsDate = document.createElement("p");
-commentsDate.setAttribute("class", "comments__timestamp");
-commentsDate.innerHTML = "02/17/2021";
-commentsData.appendChild(commentsDate);
+    const commentsAvatar = document.createElement("div");
+    commentsAvatar.setAttribute("class", "comments__avatar");
+    commentsCard.appendChild(commentsAvatar);
 
-const commentsText = document.createElement("p");
-commentsText.setAttribute("class", "comments__text");
-commentsText.innerText = "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence.Let us appreciate this for what it is and what it contains.";
-commentsContentContainer.appendChild(commentsText);
+    const commentsContentContainer = document.createElement("div");
+    commentsContentContainer.setAttribute("class", "comments__content-container")
+    commentsCard.appendChild(commentsContentContainer);
 
-const commentsDivider = document.createElement("div");
-commentsDivider.setAttribute("class", "comments__divider");
-commentsForm.appendChild(commentsDivider);
+    const commentsData = document.createElement("div");
+    commentsData.setAttribute("class", "comments__data");
+    commentsContentContainer.appendChild(commentsData);
+
+    const commentsUsername = document.createElement("p");
+    commentsUsername.setAttribute("class", "comments__username");
+    commentsUsername.innerText = arrayItem.username;
+    commentsData.appendChild(commentsUsername);
+
+    const commentsDate = document.createElement("p");
+    commentsDate.setAttribute("class", "comments__timestamp");
+    commentsDate.innerText = arrayItem.timestamp;
+    commentsData.appendChild(commentsDate);
+
+    const commentsText = document.createElement("p");
+    commentsText.setAttribute("class", "comments__text");
+    commentsText.innerText = arrayItem.text;
+    commentsContentContainer.appendChild(commentsText);
+
+    const commentsDivider = document.createElement("div");
+    commentsDivider.setAttribute("class", "comments__divider");
+    commentsForm.appendChild(commentsDivider);
+}
+
+for (let i = commentsArray.length - 1; i >= 0; i--) {
+    displayComment(commentsArray[i]);
+}
