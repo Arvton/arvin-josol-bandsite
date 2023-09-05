f2d34e9a38b999b92dce1f80ba0f35188883e584
Arvin Josol <arvin.josol@live.com>
2023-09-04 17:45:39 -0700

    add display all comments. add form submit listener - add new div section to target for clearing before displaying new comments - move variable to target comment display area to global scope - create display all comments function - add listener that:     - prevents page refresh     - grabs name and comment     - creates a formatted date     - creates a new comment object with name, date, comment     - clears the comments display     - display all comments including new one     - resets the form

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,31 +1,34 @@
 const commentsArray = [
     {
         username: "Miles Acosta",
-        timestamp: "12/20/2020",
+        date: "12/20/2020",
         text: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough."
     },
 
     {
         username: "Emilie Beach",
-        timestamp: "01/09/2021",
+        date: "01/09/2021",
         text: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day."
     },
 
     {
         username: "Connor Walton",
-        timestamp: "02/17/2021",
+        date: "02/17/2021",
         text: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains."
     }
-
 ];
 
-const displayComment = (arrayItem) => {
-    const commentsForm = document.querySelector(".comments");
+// variable for the section where elements need to be added.
+// will need for:
+// 1. displaying all comments
+// 2. clearing all comments
+const commentsDisplay = document.querySelector(".comments__display");
 
+//  function that creates one comment with needed CSS selectors
+const displayComment = (arrayItem) => {
     const commentsCard = document.createElement("div");
     commentsCard.setAttribute("class", "comments__card")
-
-    commentsForm.appendChild(commentsCard);
+    commentsDisplay.appendChild(commentsCard);
 
     const commentsAvatar = document.createElement("div");
     commentsAvatar.setAttribute("class", "comments__avatar");
@@ -46,7 +49,7 @@ const displayComment = (arrayItem) => {
 
     const commentsDate = document.createElement("p");
     commentsDate.setAttribute("class", "comments__timestamp");
-    commentsDate.innerText = arrayItem.timestamp;
+    commentsDate.innerText = arrayItem.date;
     commentsData.appendChild(commentsDate);
 
     const commentsText = document.createElement("p");
@@ -56,9 +59,44 @@ const displayComment = (arrayItem) => {
 
     const commentsDivider = document.createElement("div");
     commentsDivider.setAttribute("class", "comments__divider");
-    commentsForm.appendChild(commentsDivider);
+    commentsDisplay.appendChild(commentsDivider);
 }
 
-for (let i = commentsArray.length - 1; i >= 0; i--) {
-    displayComment(commentsArray[i]);
+// function that loops through the array of comments and displays each comment object
+const displayAllComments = (commentsArray) => {
+    for (let i = commentsArray.length - 1; i >= 0; i--) {
+        displayComment(commentsArray[i]);
+    }
 }
+
+// displays all default comments
+displayAllComments(commentsArray);
+
+// add a form submit listener
+const submitForm = document.querySelector(".comments__form")
+submitForm.addEventListener("submit", (event) => {
+    // prevents page refresh on submit
+    event.preventDefault();
+    // grabs name and comment from the form
+    const nameInput = document.getElementById("input-name");
+    const commentInput = document.getElementById("input-comment");
+    // creates a formatted date using current date
+    const fullDate = new Date();
+    const year = fullDate.getFullYear();
+    const month = fullDate.getMonth() + 1;
+    const day = fullDate.getDate();
+    const formattedDate = `${month}/${day}/${year}`;
+    // pushes name, comment, and formatted date to comment array
+    const newComment = {
+        username: nameInput.value,
+        date: formattedDate,
+        text: commentInput.value
+    }
+    commentsArray.push(newComment);
+    // clears all comments
+    commentsDisplay.innerText = "";
+    // displays all comments including new one
+    displayAllComments(commentsArray);
+    // resets the form
+    submitForm.reset();
+})
